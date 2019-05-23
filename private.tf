6f10214b8e91d7558fdd43303368dbbba85f4a35
Ruslan Zalalov <zalalov@mail.ru>
2019-05-23 22:55:00 +0300

    Bug fixed.

--- private.tf
+++ private.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "private" {
 
   lifecycle {
     # Ignore tags added by kops or kubernetes
-    ignore_changes = ["tags.%", "tags.kubernetes", "tags.SubnetType"]
+    ignore_changes = ["tags.kubernetes", "tags.SubnetType"]
   }
 }
 
